719213a79e30141819b41cf37d00e5700d2be86f
Dabeen Jeong <hello70825@gmail.com>
2022-10-11 16:04:36 +0900

    apply beforeach annotation

--- src/test/java/com/example/TDDCalculator/CalculatorTest.java
+++ src/test/java/com/example/TDDCalculator/CalculatorTest.java
@@ -1,14 +1,19 @@
 package com.example.TDDCalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    private Calculator calculator;
 
+    @BeforeEach
+    public void setup() {
+        calculator = new Calculator("3 + 5 - 2 *  3 / 4");
+    }
     @Test
     public void formulaTest() {
-        Calculator calculator = new Calculator("3 + 5 - 2 * 3 / 4");
+        setup();
     }
-
 }
