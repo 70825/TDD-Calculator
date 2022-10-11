607c0c441064c6fb97f33f6b48af94c77ffaf82c
Dabeen Jeong <hello70825@gmail.com>
2022-10-11 16:31:36 +0900

    add validator test

--- src/main/java/com/example/TDDCalculator/Calculator.java
+++ src/main/java/com/example/TDDCalculator/Calculator.java
@@ -1,5 +1,8 @@
 package com.example.TDDCalculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
     private String formula;
 
@@ -7,5 +10,38 @@ public class Calculator {
         this.formula = expression;
     }
 
+    public String getFormula() {
+        return this.formula;
+    }
 
+    public boolean validator() {
+        boolean correctFlag = true;
+
+        String[] formulaList = this.getFormula().split(" ");
+        List<Integer> integerList = new ArrayList<>();
+        List<String> operatorList = new ArrayList<>();
+
+        for(int idx = 0; idx < formulaList.length; idx++) {
+            if (idx % 2 == 0) {
+                try {
+                    int integerValue = Integer.parseInt(formulaList[idx]);
+                    integerList.add(integerValue);
+                } catch (Exception e) {
+                    correctFlag = false;
+                }
+            }
+            else {
+                String operator = formulaList[idx];
+                if (operator.equals("+") || operator.equals("-") || operator.equals("*") || operator.equals("/")) {
+                    operatorList.add(operator);
+                } else {
+                    correctFlag = false;
+                }
+            }
+        }
+
+        if (integerList.size() - operatorList.size() != 1) correctFlag = false;
+
+        return correctFlag;
+    }
 }
--- src/test/java/com/example/TDDCalculator/CalculatorTest.java
+++ src/test/java/com/example/TDDCalculator/CalculatorTest.java
@@ -3,6 +3,9 @@ package com.example.TDDCalculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
@@ -10,10 +13,30 @@ class CalculatorTest {
 
     @BeforeEach
     public void setup() {
-        calculator = new Calculator("3 + 5 - 2 *  3 / 4");
+        calculator = new Calculator("31 + 55 - 2 * 3 / 4");
     }
     @Test
     public void formulaTest() {
-        setup();
+    }
+
+    @Test
+    public void getFormula() {
+        assertEquals("31 + 55 - 2 * 3 / 4", calculator.getFormula());
+    }
+
+    @Test
+    public void validator() {
+        boolean testCorrectFlag;
+
+        testCorrectFlag = calculator.validator();
+        assertTrue(testCorrectFlag);
+
+        Calculator calculator1 = new Calculator("1 + 2 + +");
+        testCorrectFlag = calculator1.validator();
+        assertFalse(testCorrectFlag);
+
+        Calculator calculator2 = new Calculator("30 10 10");
+        testCorrectFlag = calculator2.validator();
+        assertFalse(testCorrectFlag);
     }
 }
